2eac0aa16072f697e840ec0d7ad0023905ea9ff6
Пользователь <ya@ya.ru>
2022-11-29 21:38:23 +0700

    add prog and Readme

---
+++ prog/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+//Функция перебора элементов массива с проверкой на условие
+void FillArray(string[] InitialArray, string[] NewArray, int LengthElement)  
+{
+    int index = 0;
+    for (int i = 0; i < InitialArray.Length; i++)
+        {
+            if (InitialArray[i].Length < LengthElement)
+                {
+                    NewArray[index] = InitialArray[i];
+                    index++;
+                }
+        }
+}
+
+//Функция вывода на экран 
+void PrintArray(string[] InitialArray)
+{
+    for (int i = 0; i < InitialArray.Length; i++)
+    {
+        Console.Write(InitialArray[i] + " ");
+    }
+}
+
+int LengthElement = 5;                                              //  размер искомых элементов (меньше указанного значения) 
+string[] InitialArray = {"hello", "2", "world", ";-)", "week"};     //  начальный массив
+string[] NewArray = new string[InitialArray.Length];                //  массив куда будем складывать результат
+
+FillArray(InitialArray, NewArray, LengthElement);
+PrintArray(NewArray);
